b3375c054ddf9d8bdaa2f075904c3e7e7defc41f
IdaAyuGedeDwijentari <101305025+IdaAyuGedeDwijentari@users.noreply.github.com>
2022-04-12 15:39:35 +0800

    Konfirmasi Hadir

---
+++ Pert06Lat01.java
@@ -0,0 +1,24 @@
+package pert06lat01;
+
+/**
+ *
+ * @author asus
+ */
+public class Pert06Lat01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    Luassegi3 segi3 = new Luassegi3(7,7);
+    
+    //segi3.setAlas(5);
+    //segi3.setTinggi(6);
+    
+    segi3.cetakluas();
+    
+    }
+}
+    
+
